0e3c45b66d9ee92214f3c61c6744b54fe54e0084
Gabriel Eze <gbrjnr@outlook.com>
2019-06-05 12:15:40 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,154 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+
+    <title>LandAfrica — Land Registry on the Blockchain</title>
+
+    <!-- Styles -->
+    <link href="assets/css/page.min.css" rel="stylesheet">
+    <link href="assets/css/style.css" rel="stylesheet">
+
+    <!-- Favicons -->
+    <link rel="apple-touch-icon" href="assets/img/apple-touch-icon.png">
+    <link rel="icon" href="assets/img/favicon.png">
+  </head>
+
+  <body>
+    
+    <main class="main-content">
+      <div class="section bg-gray p-0">
+        <div class="p-5 mh-fullscreen">
+          <div class="row">
+
+
+            <div class="col-md-5 col-xl-4 pl-0 pr-0">
+              <div class="sidebar px-4 py-md-0">
+
+                <nav class="navbar navbar-expand-lg navbar-dark" style="margin-top: -30px;">
+                  <div class="container">
+      
+                    <div class="navbar-left mr-4">
+                      <button class="navbar-toggler" type="button">☰</button>
+                      <a class="navbar-brand" href="#">
+                        <img class="logo-dark" src="assets/img/logo-dark.png" alt="logo">
+                        <img class="logo-light" src="assets/img/logo-light.png" alt="logo">
+                      </a>
+                    </div>
+      
+                    <section class="navbar-mobile">
+                      <nav class="nav nav-navbar mr-auto">
+                        <a class="nav-link active" href="#">About</a>
+                      </nav>
+      
+                      <div class="dropdown ml-lg-5">
+                        <span class="dropdown-toggle no-caret" data-toggle="dropdown" aria-expanded="false">
+                          <img class="avatar avatar-xs" src="assets/img/avatar/1.jpg" alt="user">
+                        </span>
+                        <div class="dropdown-menu dropdown-menu-right">
+                          <a class="dropdown-item" href="#">Profile</a>
+                          <a class="dropdown-item" href="#">Inbox</a>
+                          <a class="dropdown-item" href="#">Settings</a>
+                          <div class="dropdown-divider"></div>
+                          <a class="dropdown-item" href="#">Logout</a>
+                        </div>
+                      </div>
+                    </section>
+      
+                  </div>
+                </nav>
+
+                <hr>
+
+                <form class="form-group input-group input-round" target="#" method="GET" style="border:none!Important;">
+                  <input type="text" class="form-control" name="s" placeholder="Search">
+                  <div class="input-group-append">
+                    <span class="input-group-text"><i class="ti-search"></i></span>
+                  </div>
+                </form>
+
+                <ul class="nav nav-tabs-minimal">
+                  <li class="nav-item">
+                    <a class="nav-link active" href="#">Active</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                  </li>
+                </ul>
+
+                <hr class="mt-2">
+
+                <a class="media text-default align-items-center mb-5" href="#" data-toggle="offcanvas" data-target="#offcanvas-left">
+                  <img class="rounded w-65px mr-4" src="assets/img/thumb/4.jpg">
+                  <p class="media-body small-2 lh-4 mb-0">Thank to Maryam for joining our team</p>
+                </a>
+
+                <a class="media text-default align-items-center mb-5" href="#">
+                  <img class="rounded w-65px mr-4" src="assets/img/thumb/3.jpg">
+                  <p class="media-body small-2 lh-4 mb-0">Best practices for minimalist design</p>
+                </a>
+
+                <a class="media text-default align-items-center mb-5" href="#">
+                  <img class="rounded w-65px mr-4" src="assets/img/thumb/3.jpg">
+                  <p class="media-body small-2 lh-4 mb-0">Best practices for minimalist design</p>
+                </a>
+
+                <a class="media text-default align-items-center mb-5" href="#">
+                  <img class="rounded w-65px mr-4" src="assets/img/thumb/3.jpg">
+                  <p class="media-body small-2 lh-4 mb-0">Best practices for minimalist design</p>
+                </a>
+
+                <a class="media text-default align-items-center mb-5" href="#">
+                  <img class="rounded w-65px mr-4" src="assets/img/thumb/3.jpg">
+                  <p class="media-body small-2 lh-4 mb-0">Best practices for minimalist design</p>
+                </a>
+
+              </div>
+            </div>
+
+
+
+            <div class="col-md-7 col-xl-8 sticky" style="margin: -5px -5px;">
+              <div class="row gap-y">
+                <canvas class="w-100 mh-fullscreen" data-granim="#834d9b,#d04ed6,#1cd8d2,#93edc7" data-direction="top-bottom"></canvas>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </main>
+
+    <div id="offcanvas-left" class="offcanvas w-400" data-animation="slide-right">
+      <button type="button" class="close position-static px-0" data-dismiss="offcanvas" aria-label="Close">
+        <span aria-hidden="true">×</span>
+      </button>
+
+      <nav class="nav nav-lead flex-column my-7">
+        <a class="nav-link" href="#">Features</a>
+        <a class="nav-link" href="#">Pricing</a>
+        <a class="nav-link" href="#">Shop</a>
+        <a class="nav-link" href="#">Blog</a>
+        <a class="nav-link" href="#">About</a>
+        <a class="nav-link" href="#">Contact</a>
+      </nav>
+
+      <div class="social social-bg-gray">
+        <a class="social-facebook" href="#"><i class="fa fa-facebook"></i></a>
+        <a class="social-twitter" href="#"><i class="fa fa-twitter"></i></a>
+        <a class="social-instagram" href="#"><i class="fa fa-instagram"></i></a>
+      </div>
+    </div>
+
+    <!-- Scripts -->
+    <script src="assets/js/page.min.js"></script>
+    <script src="assets/js/script.js"></script>
+
+  </body>
+</html>
